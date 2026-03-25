31cb04043d660d2ee2d7d0f41f55418aff7b823f
Rob Simmons <rob@lean-fro.org>
2026-03-25 15:35:53 -0400

    Inline config

--- SherlockMain.lean
+++ SherlockMain.lean
@@ -1,11 +1,10 @@
 import VersoManual
 import Sherlock
 
-def config : Verso.Genre.Manual.RenderConfig where
+def main := Verso.Genre.Manual.manualMain (%doc Sherlock) (config := {
   emitTeX := false
   emitHtmlSingle := .no
   emitHtmlMulti := .immediately
   rootTocDepth := .some 2
   htmlDepth := 2
-
-def main := Verso.Genre.Manual.manualMain (%doc Sherlock) (config := config)
+})
